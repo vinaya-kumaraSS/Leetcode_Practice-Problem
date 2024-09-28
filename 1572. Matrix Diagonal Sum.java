773b4cacd22a28189eef3c898ad202a295343f48
Vinayakumara S S <121751707+vinaya-kumaraSS@users.noreply.github.com>
2024-09-28 22:45:22 +0530

    Updated

---
+++ 1572. Matrix Diagonal Sum.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        for(int i=0; i<mat.length; i++){
+            sum = sum + mat[i][i];
+            if(i != mat.length-1-i)
+                sum = sum + mat[i][mat.length-i-1];
+        }
+        return sum;
+    }
+}
