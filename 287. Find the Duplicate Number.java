4127174b682a4672f2126e4ef2d0df551d865882
Vinayakumara S S <121751707+vinaya-kumaraSS@users.noreply.github.com>
2024-10-27 14:38:38 +0530

    Updated

---
+++ 287. Find the Duplicate Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i] == nums[i+1]){
+                ans = nums[i];
+                break;
+            }
+        }
+        return ans;
+    }
+}
