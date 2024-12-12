261a74b628c124da0fb7f7e2b896cb0a1ac7f381
Vinayakumara S S <121751707+vinaya-kumaraSS@users.noreply.github.com>
2024-12-12 22:26:23 +0530

    Updated

---
+++ 3151. Special Array I.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        if(nums.length == 1)
+            return true;
+        else{
+            for(int i=1; i<nums.length; i++){
+                if(nums[i] % 2 == nums[i-1] % 2){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
