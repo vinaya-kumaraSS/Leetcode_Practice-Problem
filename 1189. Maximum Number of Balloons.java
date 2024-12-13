eaf2aa91ff0e936842570f707feb3948169b381a
Vinayakumara S S <121751707+vinaya-kumaraSS@users.noreply.github.com>
2024-12-13 22:55:06 +0530

    Updated

---
+++ 1189. Maximum Number of Balloons.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int[] count = new int[26];
+        for (char c : text.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        int b = count['b' - 'a'];
+        int a = count['a' - 'a'];
+        int l = count['l' - 'a'] / 2; 
+        int o = count['o' - 'a'] / 2; 
+        int n = count['n' - 'a'];
+
+        return Math.min(b, Math.min(a, Math.min(l, Math.min(o, n))));
+    }
+}
