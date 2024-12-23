a7bb7f4a79b0d4e3ea198f22c338a6da1dee4359
Vinayakumara S S <121751707+vinaya-kumaraSS@users.noreply.github.com>
2024-12-23 19:03:30 +0530

    Updated

---
+++ 1431. Kids With the Greatest Number of Candies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<candies.length; i++){
+            max = candies[i] > max ? candies[i] : max;
+        }
+        List<Boolean> list = new ArrayList<>();
+        for(int i=0; i<candies.length; i++){
+            if(candies[i] + extraCandies >= max){
+                list.add(true);
+            }
+            else{
+                list.add(false);
+            }
+        }
+        return list;
+    }
+}
